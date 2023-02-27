1d75b3614f9eb85deba570c7844f98faf48dbb72
jimmyGuerra <jimmy.j.guerra22@gmail.com>
2023-02-26 22:29:36 -0400

    Empezando a agregar funcionalidad para que se agreguen campos cada vez que se le de al boton de (+)

--- js/style/style.js
+++ js/style/style.js
@@ -2,9 +2,26 @@ const navbarLeft_DesktopMenu = document.querySelector('.navbar-left');
 const menuMobile = document.querySelector('.menu-mobile');
 
 const button_Menu_Mobile = document.querySelector('.button-menu-mobile');
-
 button_Menu_Mobile.addEventListener('click', toggleMobileMenu);
 
+
+//Variables de los campos de la seccion de animales
+const agregar_nuevo_campo= document.querySelector('.agregar-nuevo-campo');
+agregar_nuevo_campo.addEventListener('click', agregarCampo);
+const animales_registrados = document.querySelector('.animales-registrados');
+
+//Funcion que se encarga de mostrar y ocultar el menu en mobiles
 function toggleMobileMenu(){
     menuMobile.classList.toggle('inactive')
 }
+
+function agregarCampo(){
+    const animal_existente = document.createElement('div');
+    animal_existente.classList.add('animales-existentes');
+
+    animales_registrados.insertAdjacentElement('afterbegin',animal_existente);
+
+}
+
+
+
